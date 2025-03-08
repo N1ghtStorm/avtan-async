2ea867c1629209a75702e981171bf9d49e939d31
Dmitry <bushuevdv@mail.ru>
2025-03-08 10:59:17 -0300

    add sleep

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,3 @@
 mod spawn;
+
+pub use spawn::{Executor, sleep, hello_async};
--- src/spawn.rs
+++ src/spawn.rs
@@ -1,43 +1,164 @@
-use futures::task::noop_waker;
-use std::collections::VecDeque;
 use std::future::Future;
 use std::pin::Pin;
+use std::task::{Context, Poll, Waker};
 use std::sync::{Arc, Mutex};
-use std::task::{Context, Poll};
+use std::collections::{VecDeque, HashMap};
+use std::time::{Duration, Instant};
+use futures::task::ArcWake;
+
+// Task represents a future with its waker
+pub struct Task {
+    future: Pin<Box<dyn Future<Output = ()> + Send>>,
+    wake_time: Option<Instant>,
+}
+
+unsafe impl Send for Task {}
+unsafe impl Sync for Task {}
+
+// Implement ArcWake for Task to enable proper waking
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        // When a task is woken, push it back to the executor
+        EXECUTOR.with(|e| {
+            let executor = e.borrow();
+            executor.push_task(arc_self.clone());
+        });
+    }
+}
 
 pub struct Executor {
-    tasks: Arc<Mutex<VecDeque<Pin<Box<dyn Future<Output = ()> + Send>>>>>,
+    ready_tasks: Mutex<VecDeque<Arc<Task>>>,
+    sleeping_tasks: Mutex<HashMap<Instant, Vec<Arc<Task>>>>,
+}
+
+// Thread-local executor instance
+thread_local! {
+    static EXECUTOR: std::cell::RefCell<Executor> = std::cell::RefCell::new(Executor::new());
 }
 
 impl Executor {
-
-    #[inline(always)]
     pub fn new() -> Self {
         Executor {
-            tasks: Arc::new(Mutex::new(VecDeque::new())),
+            ready_tasks: Mutex::new(VecDeque::new()),
+            sleeping_tasks: Mutex::new(HashMap::new()),
         }
     }
 
-    #[inline(always)]
-    pub fn spawn(&self, task: impl Future<Output = ()> + Send + 'static) {
-        self.tasks.lock().unwrap().push_back(Box::pin(task));
+    fn push_task(&self, task: Arc<Task>) {
+        if let Some(wake_time) = task.wake_time {
+            if wake_time <= Instant::now() {
+                self.ready_tasks.lock().unwrap().push_back(task);
+            } else {
+                self.sleeping_tasks.lock().unwrap()
+                    .entry(wake_time)
+                    .or_default()
+                    .push(task);
+            }
+        } else {
+            self.ready_tasks.lock().unwrap().push_back(task);
+        }
     }
 
-    pub fn block_on(&self) {
-        while let Some(mut task) = self.tasks.lock().unwrap().pop_front() {
-            let waker = noop_waker();
-            let mut context = Context::from_waker(&waker);
-            match task.as_mut().poll(&mut context) {
-                Poll::Pending => {
-                    self.tasks.lock().unwrap().push_back(task);
+    pub fn spawn<F>(&self, future: F)
+    where
+        F: Future<Output = ()> + Send + 'static,
+    {
+        let task = Arc::new(Task {
+            future: Box::pin(future),
+            wake_time: None,
+        });
+        self.push_task(task);
+    }
+
+    pub fn run(&self) {
+        loop {
+            // Wake up sleeping tasks
+            let now = Instant::now();
+            let mut ready = Vec::new();
+            
+            let mut sleeping = self.sleeping_tasks.lock().unwrap();
+            let wake_times: Vec<_> = sleeping.keys().cloned().collect();
+            
+            for time in wake_times {
+                if time <= now {
+                    if let Some(tasks) = sleeping.remove(&time) {
+                        ready.extend(tasks);
+                    }
                 }
-                Poll::Ready(()) => {}
+            }
+            drop(sleeping);
+
+            // Add woken tasks to ready queue
+            self.ready_tasks.lock().unwrap().extend(ready);
+
+            // Process ready tasks
+            while let Some(task) = self.ready_tasks.lock().unwrap().pop_front() {
+                let waker = futures::task::waker_ref(&task);
+                let mut cx = Context::from_waker(&waker);
+                
+                let mut future = unsafe {
+                    // Safety: we only move the future temporarily and 
+                    // ensure it's moved back before the task is dropped
+                    // let ptr = &task.future as *const _ as *mut _;
+                    // &mut *ptr
+                    todo!()
+                };
+
+                match future.as_mut().poll(&mut cx) {
+                    Poll::Pending => {
+                        // Task will be woken up by its waker when ready
+                    }
+                    Poll::Ready(()) => {
+                        // Task is complete, it will be dropped
+                    }
+                }
+            }
+
+            if self.ready_tasks.lock().unwrap().is_empty() && 
+               self.sleeping_tasks.lock().unwrap().is_empty() {
+                break;
             }
         }
     }
 }
 
-#[inline(always)]
+// Helper function to create a sleep future
+pub struct Sleep {
+    deadline: Instant,
+}
+
+impl Future for Sleep {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
+        if Instant::now() >= self.deadline {
+            Poll::Ready(())
+        } else {
+            // Register waker to be called later
+            let task = Arc::new(Task {
+                future: Box::pin(std::future::ready(())),
+                wake_time: Some(self.deadline),
+            });
+            EXECUTOR.with(|e| {
+                e.borrow().sleeping_tasks.lock().unwrap()
+                    .entry(self.deadline)
+                    .or_default()
+                    .push(task);
+            });
+            Poll::Pending
+        }
+    }
+}
+
+pub fn sleep(duration: Duration) -> Sleep {
+    Sleep {
+        deadline: Instant::now() + duration,
+    }
+}
+
+// Example async function
 pub async fn hello_async() {
-    println!("Hello from async runtime!");
+    println!("Hello");
+    sleep(Duration::from_secs(1)).await;
+    println!("World!");
 }
